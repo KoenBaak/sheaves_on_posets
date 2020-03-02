fc87e9296d3eee3b840a755f65840806848453ce
KoenBaak <38015449+KoenBaak@users.noreply.github.com>
2020-03-02 16:24:55 +0100

    Add files via upload

--- sheaves_on_posets/sheaf_homset.py
+++ sheaves_on_posets/sheaf_homset.py
@@ -10,8 +10,9 @@ class LocFreeSheafHomset(Parent):
     def __init__(self, sheaf1, sheaf2):
         Parent.__init__(self, category=Sets())
         self._domain = sheaf1
-        self._codomain = sheaf2
-        print("in homset __init__")     
+        self._codomain = sheaf2     
+        self._base_ring = sheaf1._base_ring
+        self._domain_poset = sheaf1._domain_poset
          
     def domain(self):
         return self._domain
@@ -19,9 +20,12 @@ class LocFreeSheafHomset(Parent):
     def codomain(self):
         return self._codomain
     
-    def __call__(self, component_dict):
-        print("in homset __call__")
-        return self.element_class(self, component_dict)
+    def __call__(self, component_dict, name = "sheaf morphism"):
+        mor = self.element_class(self, component_dict, name)
+        for r in self._domain_poset.cover_relations():
+            if not self._codomain.restriction(r[0], r[1]).matrix() * mor.component[r[0]].matrix() == mor.component[r[1]].matrix() * self._domain.restriction(r[0], r[1]):
+                raise ValueError("input does not define a morphism of sheaves")
+        return mor
     
     def _repr_(self):
         return "Set of morphisms from {} to {}".format(self._domain, self._codomain)
--- sheaves_on_posets/sheaf_morphism.py
+++ sheaves_on_posets/sheaf_morphism.py
@@ -1,9 +1,40 @@
 
 from sage.structure.element import Element
 
+from sage.matrix.special import identity_matrix
+
+
 class LocFreeSheafMorphism(Element):
     
-    def __init__(self, parent, component_dict):
+    def __init__(self, parent, component_dict, name="sheaf morpism"):
         Element.__init__(self, parent)
+        self._parent = parent
         self._components = component_dict
-        print('in morphism init')
+        self._base_ring = self._parent._base_ring
+        self._domain_poset = self._parent._domain_poset
+        self._name = name
+        
+    def domain(self):
+        return self._parent.domain()
+    
+    def codomain(self):
+        return self._parent.codomain()
+    
+    def component(self, point):
+        h = Hom(self.domain().stalk(point), self.codomain().stalk(point))
+        if self._components[point] == 0:
+            phi = h.zero()
+        elif self._components[point] == 1:
+            phi = h(identity_marix(self._base_ring, self.domain()._stalk_dict[point]))
+        else:
+            phi = h(self._components[point])    
+        phi._name = "Component of {} at {}".format(self._name, point)
+        
+    def __getitem__(self, i):
+        return self.component(i)
+    
+    def _latex_(self):
+        return r'\mbox{' + str(self) + r'}'
+    
+    def _repr_(self):
+        return "Generic Morphism of Locally Free Sheaves of Modules over {} on {}\n From: {}\n To: {}".format(self._base_ring, self._domain_poset, self.domain(), self.codomain())
