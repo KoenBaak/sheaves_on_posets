13f559f27af028f54ef54e41c31d60a86dc5f55d
KoenBaak <38015449+KoenBaak@users.noreply.github.com>
2020-03-02 15:36:59 +0100

    Delete sheaf_morphism.py

--- sheaf_morphism.py
+++
@@ -1,9 +0,0 @@
-
-from sage.categories.morphism import Morphism
-
-class LocFreeSheafMorphism(Morphism):
-    
-    def __init__(self, parent, component_dict):
-        Morphism.__init__(self, parent)
-        self._components = component_dict
-        print('in morphism init')
