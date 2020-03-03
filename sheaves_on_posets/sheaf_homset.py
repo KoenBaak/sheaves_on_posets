502471390e7e5de473b2a6d0adc667cf54fa2143
KoenBaak <38015449+KoenBaak@users.noreply.github.com>
2020-03-03 15:43:55 +0100

    Add files via upload

--- sheaves_on_posets/sheaf_homset.py
+++ sheaves_on_posets/sheaf_homset.py
@@ -33,8 +33,8 @@ class LocFreeSheafHomset(Parent):
             print('component {} : {}'.format(r[0], mor.component_matrix(r[0])))
             print("domain restriction {} : {}".format(r, self._codomain.restriction(r[0], r[1]).matrix()))
             print('component {} : {}'.format(r[1], mor.component_matrix(r[1])))
-            if not self._codomain.restriction(r[0], r[1]).matrix() * mor.component_matrix(r[0]) == mor.component_matrix(r[1]) * self._domain.restriction(r[0], r[1]):
-                print("fail")
+            #if not self._codomain.restriction(r[0], r[1]).matrix() * mor.component_matrix(r[0]) == mor.component_matrix(r[1]) * self._domain.restriction(r[0], r[1]):
+             #   print("fail")
                 #raise ValueError("input does not define a morphism of sheaves")
         return mor
     
